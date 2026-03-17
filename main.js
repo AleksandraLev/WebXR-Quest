9e7137cb57980c34a9b8db139f9c8bb79392c95e
AleksandraLev <sasha.levashova@mail.ru>
2026-03-17 16:25:45 +0300

    UI должен быть над AR-сценой.

--- main.js
+++ main.js
@@ -36,8 +36,15 @@ function init(){
 
     document.body.appendChild(renderer.domElement);
 
-    document.body.appendChild(ARButton.createButton(renderer,{requiredFeatures:["hit-test"]}));
-
+    //document.body.appendChild(ARButton.createButton(renderer,{requiredFeatures:["hit-test"]}));
+    document.body.appendChild(
+        ARButton.createButton(renderer, {
+            requiredFeatures: ["hit-test"],
+            optionalFeatures: ["dom-overlay"],
+            domOverlay: { root: document.body }
+        })
+    );
+    
     const light=new THREE.HemisphereLight(0xffffff,0xbbbbff,1);
     scene.add(light);
 
